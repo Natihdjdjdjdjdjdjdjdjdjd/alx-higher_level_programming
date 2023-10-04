011757334ae2ca0a299d972a7655f3b043922acd
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2023-10-04 05:53:06 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(97, 123):
-    if alphabet != (101) and alphabet != (113):
-        print("{:c}".format(chr(alphabet)), end="")
+for alphabet in range(ord('a'), ord('z') + 1):
+    if alphabet != ord('q') and alphabet != ord('e'):
+        print("{:c}".format(alphabet), end="")
