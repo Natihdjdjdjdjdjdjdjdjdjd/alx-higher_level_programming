4ae1034d43cec10000b34ed97da6901499eb9597
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2023-10-04 19:44:31 -0700

    13-insert_number.c

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * insert_node - the function that that ensert node
+ * @head: A pointr that link heder
+ * @number: the value that enter node.
+ *
+ * Return: If fails - NULL.Otherwise - the new node.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
