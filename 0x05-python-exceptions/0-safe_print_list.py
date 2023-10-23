2f5f4981b02b0e353ee0b4dc5f45807a4f48feb7
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2023-10-23 11:16:10 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    back = 0
+    try:
+        while back is not x:
+            print(my_list[back], end='')
+            back += 1
+    except IndexError:
+        None
+    print()
+    return back
