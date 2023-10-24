57f3916e4a15bf128bc4666d7e4c7b6ad2327d45
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2023-10-24 13:40:54 -0700

    101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,89 +1,65 @@
 #!/usr/bin/python3
-"""let create the class that define the single link"""
+"""lets define that the value of  class Square."""
 
 
-class Node:
-    """ let create a node in single link
-    """
+class Square:
+    """creating  a square."""
 
-    def __init__(self, data, next_node=None):
-        """Initialize Node
-        """
-        self.data = data
-        self.next_node = next_node
+    def __init__(self, size=0, position=(0, 0)):
+        """intializing of the class"""
+        self.size = size
+        self.position = position
 
     @property
-    def data(self):
-        """data getter
-        returns:
-            data (int)
-        """
-        return self.__data
+    def size(self):
+        """Let us set the current size of the square."""
+        return (self.__size)
 
-    @data.setter
-    def data(self, value):
-        """data setter
-        args:
-            value (int): value to set
-        returns:
-            None
-        """
-        if type(value) != int:
-            raise TypeError("data must be an integer")
-        self.__data = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     @property
-    def next_node(self):
-        """data getter
-        returns:
-            data (int)
-        """
-        return self.__next_node
+    def position(self):
+        """Let us set the current position of the square."""
+        return (self.__position)
 
-    @next_node.setter
-    def next_node(self, value):
-        """data setter
-        args:
-            value (Node): value to set
-        returns:
-            None
-        """
-        if not isinstance(value, Node) and value is not None:
-            raise TypeError("next_node must be a Node object")
-        self.__next_node = value
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
-class SinglyLinkedList:
-    """Singly linked list class
-    """
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
 
-    def __init__(self):
-        """Initialize linked list"""
-        self.__head = None
-
-    def sorted_insert(self, value):
-        """insert node in coorect sorted position"""
-        new = Node(value)
-        if self.__head is None:
-            new.next_node = None
-            self.__head = new
-        elif self.__head.data > value:
-            new.next_node = self.__head
-            self.__head = new
-        else:
-            tmp = self.__head
-            while (tmp.next_node is not None and
-                    tmp.next_node.data < value):
-                tmp = tmp.next_node
-
-            new.next_node = tmp.next_node
-            tmp.next_node = new
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for m in range(0, self.__position[0])]
+            [print("#", end="") for n in range(0, self.__size)]
+            print("")
 
     def __str__(self):
-        """let definetrepresentation of a SinglyLinkedList."""
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
-        return ('\n'.join(values))
+        """Define the print() representation of a Square."""
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for m in range(0, self.__position[0])]
+            [print("#", end="") for n in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
