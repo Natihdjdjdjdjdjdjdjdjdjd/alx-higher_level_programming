4f454282a71188e45ec938a6a1ad555032a00521
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2023-10-31 12:51:34 -0700

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 class LockedClass:
+
     __slots__ = ["first_name"]
+
+    def __init__(self):
+        """Creates new instances of Locked Class."""
+
+        self.first_name = "first_name"
