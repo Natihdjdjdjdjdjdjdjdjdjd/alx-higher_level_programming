c0a11f14dae7682255db38217409c2aa2307f5a0
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-01-30 10:00:20 -0800

    4-hbtn_status.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 """
-script that takes in a URL and an email, sends a POST request to the passed
-URL with the email as a parameter, and displays the body of the response
-(decoded in utf-8).
+script that takes in a URL and an email, sends a POST
 """
 import urllib.request
+import urllib.parse
 import sys
-import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    value = {
+    param = {
         "email": email
     }
-    query_string = urllib.parse.urlencode(value)
+    query_string = urllib.parse.urlencode(param)
     data = query_string.encode("ascii")
-    req_r = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req_r) as response:
-        response_text = response.read().decode("utf-8")
-        print(response_text)
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+
+        text_resp = response.read().decode("utf-8")
+        print(text_resp)
